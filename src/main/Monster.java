79b886c06da02e7ec302f61d511f20f6006c5f03
joshuapinpin <joshuapinpin421@gmail.com>
2025-08-20 15:59:37 +1200

    Added Monster States in Monster.java

--- src/main/Monster.java
+++ src/main/Monster.java
@@ -7,17 +7,17 @@ import imgs.Img;
 
 class Monster implements Entity{
   private Point location;
+  private MonsterState monsterState = new AwakeState();
+  
+  Monster(Point location){ this.location= location; }
+  
   public Point location(){ return location; }
   public void location(Point p){ location = p; }
-  Monster(Point location){ this.location= location; }
   public double speed(){ return 0.05d; }
-
-  public void ping(Model m){
-    var arrow= m.camera().location().distance(location);
-    double size= arrow.size();
-    arrow = arrow.times(speed() / size);
-    location = location.add(arrow); 
-    if (size < 0.6d){ m.onGameOver(); }
+  
+  public void ping(Model m){monsterState.ping(m);}
+  public void draw(Graphics g, Point center, Dimension size) {
+  	monsterState.draw(g, center, size);
   }
  
   public double chaseTarget(Monster outer, Point target){
@@ -27,7 +27,55 @@ class Monster implements Entity{
     outer.location(outer.location().add(arrow));
     return size;
   }
-  public void draw(Graphics g, Point center, Dimension size) {
-    drawImg(Img.AwakeMonster.image, g, center, size);
+  
+  private interface MonsterState{
+  	void ping(Model m);
+  	void draw(Graphics g, Point center, Dimension size);
+  }
+  
+  private class AwakeState implements MonsterState{
+
+		@Override
+		public void ping(Model m) {
+			var arrow= m.camera().location().distance(location);
+	    double size= arrow.size();
+	    arrow = arrow.times(speed() / size);
+	    location = location.add(arrow); 
+	    if (size < 0.6d){ m.onGameOver(); }
+		}
+
+		@Override
+		public void draw(Graphics g, Point center, Dimension size) {
+			drawImg(Img.AwakeMonster.image, g, center, size);
+		}
+  }
+  private class SleepState implements MonsterState{
+
+		@Override
+		public void ping(Model m) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void draw(Graphics g, Point center, Dimension size) {
+			// TODO Auto-generated method stub
+			
+		}
+  }
+  private class DeadState implements MonsterState{
+
+		@Override
+		public void ping(Model m) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void draw(Graphics g, Point center, Dimension size) {
+			// TODO Auto-generated method stub
+			
+		}
+  	
   }
 }
